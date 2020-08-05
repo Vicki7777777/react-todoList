c0c1df1a2b61f011b1c609e8b53272127799a07c
Vicki <458044962@qq.com>
2020-08-05 15:12:04 +0800

    [react-todolist][vicki] feature :  add router

--- src/App.js
+++ src/App.js
@@ -2,57 +2,54 @@ import React from 'react';
 import './App.css';
 import TodoListContainer from '../src/containers/TodoListContainer'
 import TodoFormContainer from '../src/containers/TodoFormContainer'
-// import {
-//     HashRouter as Router,
-//     Route,
-//     Link
-// } from 'react-router-dom';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
 import TodoDoneContainer from "./containers/TodoDoneContainer";
 
+//import Switch from "antd";
+
 // function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <TodoListContainer />
-//         <TodoFormContainer />
-//       </header>
-//     </div>
-//   );
+//     return (
+//         <div className="App">
+//             <header className="App-header">
+//                 <TodoListContainer/>
+//                 <TodoFormContainer/>
+//             </header>
+//         </div>
+//     );
 // }
 
 
 function App() {
     return (
-        // <Router>
-        //     <div>
-        //         <ul>
-        //             <li><Link to="/">ToDoHome</Link></li>
-        //             <li><Link to="/done">PlayToDo</Link></li>
-        //             <li><Link to="/undone">ToDoDone</Link></li>
-        //         </ul>
-        //         <Switch>
-        //             <Route exact path="/">
-        //                 <App />
-        //             </Route>
-        //             <Route path="/done">
-        //                 <TodoListContainer  />
-        //             </Route>
-        //             <Route path="/undone">
-        //                 <TodoDoneContainer />
-        //             </Route>
-        //             <Route exact path="/undone">
-        //                 <Todo />
-        //             </Route>
-        //         </Switch>
-        //     </div>
-        // </Router>
-        <div className="App">
-            <header className="App-header">
-                <TodoListContainer />
-                <TodoFormContainer />
-                <TodoDoneContainer />
-            </header>
-        </div>
+        <Router>
+            <div>
+                <ul>
+                    <li><Link to="/">ToDoHome</Link></li>
+                    <li><Link to="/undone">ToDoDone</Link></li>
+                </ul>
+                <Switch>
+                    <Route exact path="/">
+                        <TodoListContainer/>
+                        <TodoFormContainer/>
+                    </Route>
+                    <Route path="/undone">
+                        <TodoDoneContainer/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+        // <div className="App">
+        //     <header className="App-header">
+        //         <TodoListContainer />
+        //         <TodoFormContainer />
+        //         <TodoDoneContainer />
+        //     </header>
+        // </div>
     );
 }
 
